3da3c2a76030de852d317f3c936e0b1bd06987fb
KaVin S <kavinadithya3@gmail.com>
2024-10-14 16:07:55 +0530

    Creating entities and importing packages and organizing modules

--- E-Commerce/Entity/KeyBoard.py
+++ E-Commerce/Entity/KeyBoard.py
@@ -1,10 +1,12 @@
-import Product
+from .Product import Product
 
 class KeyBoard(Product):
     def __init__(self, name, quantity, price, brand):
-        super().__init__(name, quantity, price)
-        self.__brand = brand
-
+        super().__init__(name, quantity, price, brand)
 
     def __str__(self) -> str:
-        return 'KeyBoard ' +  self.product_details() + '\n Brand ' + self.brand + '\n Quantity : ' + self.quantity
+        return ('\n ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~'
+                + '\n | !^! KeyBoard ' +  self.product_details()
+                + '\n | Brand : ' + self.brand
+                + '\n | Quantity : ' + str(self.quantity)
+                + '\n ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ')
